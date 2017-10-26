086a2c7259fe624061e87816c70cfb9c8f039101
lixufeng <617105706@qq.com>
2017-10-26 09:57:43 +0800

    end

--- js/style.js
+++ js/style.js
@@ -13,7 +13,7 @@ $(function() {
 	});
 
 	/*---------------------------------------------------------------------------------------------------------------------*/
-	$('.icon_videoPlay').on('click', function() {
+	/*$('.icon_videoPlay').on('click', function() {
 		// alert('a')
 		var close = '<span class="close" title="关闭视频"></span>'
 		var wlg_v = '<embed class="wlg_v" width="100%" height="100%" src="http://yuntv.letv.com/bcloud.swf?uu=661c07e19e&vu=0d11e61e4f&auto_play=1&gpcflag=1&lang=zh_CN" allowscriptaccess="always" allowfullscreen="true" wmode="opaque">';
@@ -22,7 +22,69 @@ $(function() {
 		$('.video .close').on('click', function() {
 			$('.close, .wlg_v').remove();
 		})
+
+	});*/
+
+
+
+	$(".content_5").mCustomScrollbar({
+		horizontalScroll: true,
+		mouseWheel: {
+			enable: false
+		},
+		advanced: {
+			autoExpandHorizontalScroll: true
+		},
+		theme: "dark-thin"
+	});
+	$('.content_5').mCustomScrollbar('scrollTo', 'left');
+
+	// 选择战队
+	argumentsTab('.tabList li');
+	// 赛事进程
+	argumentsTab('.tabEvent_list li', '.tabBox .box');
+
+	$(document).on("click", ".vBtn", function() {
+		var url = $(this).attr("data-url");
+		var vuId = getQueryString(url).vu;
+		var _ww = $("#wrapper").width(),
+			video_w = 640,
+			video_h = 360,
+			player = new CloudVodPlayer();
+
+		if (_ww < 640) {
+			video_w = _ww;
+			video_h = _ww * 360 / 640
+		}
+		dialog.showInfo("<div id='vInfo' class='vInfo' style='height:" + video_h + "px'></div>");
+		$("#maskLayer").addClass("close");
+		player.init({
+			"uu": "661c07e19e",
+			"vu": vuId,
+			"auto_play": 1,
+			"pu": "5dd4aa0351",
+			"width": video_w,
+			"height": video_h,
+			"lang": "zh_CN"
+		}, "vInfo");
 	})
+
+	function getQueryString(url) {
+		var qs = url;
+		var args = {};
+		var items = qs.split("&");
+		var ite = null;
+		var name = null;
+		var value = null;
+		for (var i = 0; i < items.length; i++) {
+			//ite=items[i].split("=");
+			var firstEqual = items[i].indexOf("=")
+			name = items[i].substring(0, firstEqual);
+			value = items[i].substring(firstEqual + 1);
+			args[name] = value;
+		}
+		return args;
+	}
 })
 
 function navClick(obj, btn) {
